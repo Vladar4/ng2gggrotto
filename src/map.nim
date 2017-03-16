ff90db35d1331ca0c4107d4878efc6f69bec0115
Vladar4 <vladar4@gmail.com>
2017-03-16 21:50:05 +0200

    random map generation

--- src/map.nim
+++ src/map.nim
@@ -17,7 +17,7 @@ type
 
   Tile = enum
     flr # floor
-    u1
+    non
     u2
     u3
     wU  # wall up
@@ -34,7 +34,7 @@ type
     cDR # corner down-right
 
   Triad = enum
-    UD, LR, UL, UR, DL, DR, UDL, UDR, ULR, DLR, UDLR
+    NONE, UD, LR, UL, UR, DL, DR, UDL, UDR, ULR, DLR, UDLR
 
 
 converter toInt(tile: Tile): int =
@@ -43,6 +43,9 @@ converter toInt(tile: Tile): int =
 
 const
   Triads = [
+    [ [non, non, non],  # NONE
+      [non, non, non],
+      [non, non, non] ],
     [ [wL , flr, wR ],  # UD
       [wL , flr, wR ],
       [wL , flr, wR ] ],
@@ -77,15 +80,15 @@ const
       [flr, flr, flr],
       [tDL, flr, tDR] ]
   ]
-  TriadsAll = {UD, LR, UL, UR, DL, DR, UDL, UDR, ULR, DLR, UDLR}
+  TriadsAll = {NONE, UD, LR, UL, UR, DL, DR, UDL, UDR, ULR, DLR, UDLR}
   TriadsU   = {UD, UL, UR, UDL, UDR, ULR, UDLR}
   TriadsD   = {UD, DL, DR, UDL, UDR, DLR, UDLR}
   TriadsL   = {LR, UL, DL, UDL, ULR, DLR, UDLR}
   TriadsR   = {LR, UR, DR, UDR, ULR, DLR, UDLR}
-  TriadsNoU = {LR, DL, DR, DLR}
-  TriadsNoD = {LR, UL, UR, ULR}
-  TriadsNoL = {UD, UR, DR, UDR}
-  TriadsNoR = {UD, UL, DL, UDL}
+  TriadsNoU = {NONE, LR, DL, DR, DLR}
+  TriadsNoD = {NONE, LR, UL, UR, ULR}
+  TriadsNoL = {NONE, UD, UR, DR, UDR}
+  TriadsNoR = {NONE, UD, UL, DL, UDL}
   MapTileWidth    = 48
   MapTileHeight   = 30
   MapTriadWidth   = MapTileWidth  div 3
@@ -123,18 +126,35 @@ proc init*(map: Map) =
   map.graphic = gfxData["tiles"]
   map.initSprite SpriteDim, SpriteOffset
   map.clear()
+  var t: array[MapTriadHeight, array[MapTriadWidth, Triad]]
   for y in 0..<MapTriadHeight:
     for x in 0..<MapTriadWidth:
       var choice = TriadsAll
+      # x
       if x == 0:
         choice = choice - TriadsL
-      if y == 0:
-        choice = choice - TriadsU
+      elif t[y][x-1] in TriadsR:
+          choice = choice - TriadsNoL
+      else:
+        echo choice
+        choice = choice - TriadsL
       if x == MapTriadWidth - 1:
         choice = choice - TriadsR
+      # y
+      if y == 0:
+        choice = choice - TriadsU
+      elif t[y-1][x] in TriadsD:
+        choice = choice - TriadsNoU
+      else:
+        echo choice
+        choice = choice - TriadsU
       if y == MapTriadHeight - 1:
         choice = choice - TriadsD
-      map.set((x, y), random choice)
+      t[y][x] = random choice
+
+  for y in 0..<MapTriadHeight:
+    for x in 0..<MapTriadWidth:
+      map.set((x, y), t[y][x])
 
 
 proc free*(map: Map) =
