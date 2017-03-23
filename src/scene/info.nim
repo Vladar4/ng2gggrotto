60e695f8b446ca7d905162547aef0eb3e62df337
Vladar4 <vladar4@gmail.com>
2017-03-23 12:42:46 +0200

    added info text

--- src/scene/info.nim
+++ src/scene/info.nim
@@ -16,12 +16,55 @@ import
 
 type
   InfoScene = ref object of Scene
-    infoText: TextGraphic
+    text, infoText: TextGraphic
     btnBack: MenuButton
 
 
 proc init*(scene: InfoScene) =
   init Scene(scene)
+
+  # pics
+  let
+    egg = newEntity()
+    enemy = newEntity()
+    follower = newEntity()
+  egg.graphic = gfxData["egg"]
+  egg.centrify(ver = VAlign.top)
+  enemy.graphic = gfxData["enemy1"]
+  enemy.initSprite(SpriteDim)
+  discard enemy.addAnimation("a", toSeq(4..7))
+  enemy.play("a", -1)
+  enemy.centrify(ver = VAlign.top)
+  follower.graphic = gfxData["follower"]
+  follower.initSprite(SpriteDim)
+  discard follower.addAnimation("a", toSeq(4..7))
+  follower.play("a", -1)
+  follower.centrify(ver = VAlign.top)
+  # y: 224, 256, 288, 320, 352, 384, 416, 448, 480, 512
+  egg.pos = (234.0, 288.0)
+  enemy.pos = (186.0, 320.0)
+  follower.pos = (154.0, 384.0)
+  scene.add egg
+  scene.add enemy
+  scene.add follower
+
+  # text
+  scene.text = newTextGraphic defaultFont
+  scene.text.lines = [
+    "            HOW TO PLAY",
+    "To win the game you must collect",
+    "all the eggs   , while evading",
+    "the yetis   .",
+    "Each egg gives you one more",
+    "partner   , increasing your",
+    "score multiplier.",
+    "The more partners you have,",
+    "the faster you are."
+  ]
+  let t = newEntity()
+  t.graphic = scene.text
+  t.pos = (16.0, 212.0)
+  scene.add t
 
   # info
   scene.infoText = newTextGraphic bonusFont
@@ -38,7 +81,7 @@ proc init*(scene: InfoScene) =
     step = (0.0, 64.0)
     width = 6
 
-  scene.btnBack = newMenuButton(center + step * 3.0, width,
+  scene.btnBack = newMenuButton(center + step * 4.25, width,
     proc(btn: MenuButton) {.locks:0.} = game.scene = titleScene,
     "BACK")
   scene.btnBack.centrify()
@@ -54,6 +97,7 @@ proc init*(scene: InfoScene) =
 
 
 proc free*(scene: InfoScene) =
+  free scene.text
   free scene.infoText
   free scene.btnBack
   freeData()
