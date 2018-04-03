15b4e614debec29f34ce57ab540d812c6edac233
Vladar4 <vladar4@gmail.com>
2018-04-03 21:23:53 +0300

    updated appimage building scripts

--- appimage-x86_64.sh
+++ appimage-x86_64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Configured for Ubuntu 12.04.5 LTS (x86_64)
+# Configured for Ubuntu 16.04 LTS (x86_64)
 
 NAME="Glorious Glacier Grotto"
 PROJ="gggrotto"
@@ -12,12 +12,11 @@ DEPS="\
 /usr/local/lib/libSDL2_image.so \
 /usr/local/lib/libSDL2_mixer.so \
 /usr/local/lib/libSDL2_ttf.so \
-/usr/lib/$ARCH-linux-gnu/libfreetype.so \
+/lib/$ARCH-linux-gnu/libpng16.so.16 \
 /usr/lib/$ARCH-linux-gnu/libogg.so.0 \
-/lib/$ARCH-linux-gnu/libpng12.so.0 \
+/usr/lib/$ARCH-linux-gnu/libsndio.so.6.1 \
 /usr/lib/$ARCH-linux-gnu/libvorbis.so.0 \
-/usr/lib/$ARCH-linux-gnu/libvorbisfile.so.3 \
-/lib/$ARCH-linux-gnu/libz.so.1"
+/usr/lib/$ARCH-linux-gnu/libvorbisfile.so.3"
 
 rm -rf "$PROJ.AppDir"
 mkdir -p "$PROJ.AppDir/usr/bin"
@@ -37,11 +36,11 @@ Exec=$PROJ
 Categories=Game;
 Icon=$PROJ" > "$PROJ.desktop"
 
-wget -O AppRun https://github.com/probonopd/AppImageKit/releases/download/8/AppRun-x86_64
+wget -O AppRun https://github.com/probonopd/AppImageKit/releases/download/10/AppRun-x86_64
 chmod +x AppRun
 
 cd ..
-wget -N https://github.com/probonopd/AppImageKit/releases/download/8/appimagetool-x86_64.AppImage
+wget -N https://github.com/probonopd/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage "$PROJ.AppDir" "$PROJ-$ARCH.AppImage"
 
